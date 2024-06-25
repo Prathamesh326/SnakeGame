3e94cdf2f91586cdb2a0afa8ef9b68afd4d75c09
Prathamesh326 <110224953+Prathamesh326@users.noreply.github.com>
2024-06-25 12:38:14 +0530

    Update score.py

--- score.py
+++ score.py
@@ -5,21 +5,28 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open('data.txt') as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
-        self.updatescore()
+        self.update_score()
         self.hideturtle()
 
-    def updatescore(self):
-        self.write(arg=f"Score : {self.score}", move=False, align="center", font=('Arial', 20, 'normal'))
+    def update_score(self):
+        self.clear()
+        self.write(arg=f"Score : {self.score}  High Score : {self.high_score}", move=False, align="center",
+                   font=('Arial', 20, 'normal'))
 
-    def gameover(self):
-        self.goto(0,0)
-        self.write(arg="GAME OVER", move=False, align="center", font=('Arial', 15, 'normal'))
+    def reset_score(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open('data.txt', 'w') as data:
+                data.write(f'{self.high_score}')
 
+        self.score = 0
+        self.update_score()
 
     def calculate_score(self):
         self.score += 1
-        self.clear()
-        self.updatescore()
+        self.update_score()
